462e0f832c19cdeccdd02024dd2b3a8ace861222
Cho <whghdrka@naver.com>
2019-06-28 19:19:29 +0900

    QnaSql.xml , QnaService.java 추가

---
+++ ForkcookProject/src/main/java/spring/service/QnaService.java
@@ -0,0 +1,46 @@
+package spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import spring.data.QnaDao;
+import spring.data.QnaDto;
+
+@Service
+public class QnaService {
+	
+	@Autowired
+	private QnaDao dao;
+	
+	public int getTotalCount()
+	{
+		return dao.getTotalCount();
+	}
+
+	public void insertBoard(QnaDto dto)
+	{
+		dao.insertQna(dto);
+	}
+	
+	public List<QnaDto> getList(int start,int end)
+	{
+		return dao.getList(start, end);
+	}
+	
+	public QnaDto getData(int num)
+	{
+		return dao.getData(num);
+	}
+	
+	public void qnaUpdate(QnaDto dto)
+	{
+		dao.qnaUpdate(dto);
+	}
+	
+	public void qnaDelete(int num)
+	{
+		dao.qnaDelete(num);
+	}
+}
